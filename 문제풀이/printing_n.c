a06bdfa3f02364d31f6b7c2ca874616d58e57f80
jinnyfruit <jinnyfruit0527@gmail.com>
2021-01-15 19:53:42 +0900

    printing n

---
+++ 문제풀이/printing_n.c
@@ -0,0 +1,15 @@
+/*
+ file name: printing n
+ detail: 
+ made_by: jinnyfruit
+*/
+#include <stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    int i;
+    for(i=1;i<=num;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
